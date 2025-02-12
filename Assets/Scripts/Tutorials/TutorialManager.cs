ae70178aeb16d0d7fdc938088d5bb7fa46e54054
Tonrkz <tonrak.5@hotmail.com>
2025-02-12 22:09:59 +0700

    Build Command Tutorial

--- Assets/Scripts/Tutorials/TutorialManager.cs
+++ Assets/Scripts/Tutorials/TutorialManager.cs
@@ -18,15 +18,22 @@ public class TutorialManager : MonoBehaviour {
     [Header("Attributes")]
     [SerializeField] Enum_TutorialState state = Enum_TutorialState.Start;
     List<string> startTutorialMessages = new List<string> {
-        "Welcome to Kingdom Kome!",
-        "Because you are a new king to this kingdom, I am going to teach you the basics of managing your army.",
-        "Let's get started!"
+        $"Welcome to Kingdom Kome!",
+        $"Because you are a new king to this kingdom, I am going to teach you the basics of managing your army.",
+        $"Let's get started!"
     };
     List<string> playerMovementTutorialMessages = new List<string> {
-        "First, you need to move your king to the desired location.",
-        "Use the arrow keys to move your king.",
-        "Now, move to the highlighted location.",
-        "Good job! You have successfully moved into the desired location."
+        $"First, you need to move your king to the desired location.",
+        $"Use the arrow keys to move your king.",
+        $"Now, move to the highlighted location.",
+        $"Good job! You have successfully moved into the desired location."
+    };
+    List<string> buildCommandTutorialMessages = new List<string> {
+        $"Let's plan our defense.",
+        $"In Kingdom Kome, you have to build a campfire first.",
+        $"To build a campfire, type 'build' and press enter.",
+        $"Now, try building a campfire by typing 'build' and press enter.",
+        $"Good job! You have successfully built a campfire."
     };
 
     [Header("Debug")]
@@ -57,6 +64,7 @@ public class TutorialManager : MonoBehaviour {
     /// Initiate tutorial based on the current state
     /// </summary>
     void OnInitiateTutorial() {
+        pressEnterToContinueText.SetActive(true);
         IsConditionMeet = false;
         InputStateManager.instance.GameInputState = Enum_GameInputState.Tutorial;
         switch (state) {
@@ -67,6 +75,7 @@ public class TutorialManager : MonoBehaviour {
                 UserInterfaceManager.instance.ChangeTextMessage(tutorialText, playerMovementTutorialMessages[tutorialTextIndex]);
                 break;
             case Enum_TutorialState.BuildCommand:
+                UserInterfaceManager.instance.ChangeTextMessage(tutorialText, buildCommandTutorialMessages[tutorialTextIndex]);
                 break;
             case Enum_TutorialState.ModeSwitching:
                 break;
@@ -113,6 +122,7 @@ public class TutorialManager : MonoBehaviour {
                     }
                 }
                 break;
+
             case Enum_TutorialState.PlayerMovement:
                 if (Input.GetKeyDown(KeyCode.Return)) {
                     if (tutorialTextIndex < playerMovementTutorialMessages.Count - 3) {
@@ -121,8 +131,12 @@ public class TutorialManager : MonoBehaviour {
                     }
                     else {
                         UserInterfaceManager.instance.ChangeTextMessage(tutorialText, playerMovementTutorialMessages[playerMovementTutorialMessages.Count - 2]);
-                        UserInterfaceManager.instance.ToggleUI(pressEnterToContinueText);
+                        pressEnterToContinueText.SetActive(false);
                         InputStateManager.instance.GameInputState = Enum_GameInputState.CommandMode;
+                    }
+                    if (IsConditionMeet) {
+                        UpdateStep();
+                        return;
                     }
                 }
                 if (Vector3.Distance(player.transform.position, playerMovementTutorialTargetLocation.transform.position) < 1.5f) {
@@ -131,8 +145,30 @@ public class TutorialManager : MonoBehaviour {
                     }
                 }
                 break;
+
             case Enum_TutorialState.BuildCommand:
+                if (Input.GetKeyDown(KeyCode.Return)) {
+                    if (tutorialTextIndex < buildCommandTutorialMessages.Count - 3) {
+                        tutorialTextIndex++;
+                        UserInterfaceManager.instance.ChangeTextMessage(tutorialText, buildCommandTutorialMessages[tutorialTextIndex]);
+                    }
+                    else {
+                        UserInterfaceManager.instance.ChangeTextMessage(tutorialText, buildCommandTutorialMessages[buildCommandTutorialMessages.Count - 2]);
+                        pressEnterToContinueText.SetActive(false);
+                        InputStateManager.instance.GameInputState = Enum_GameInputState.CommandMode;
+                    }
+                    if (IsConditionMeet) {
+                        UpdateStep();
+                        return;
+                    }
+                }
+                if (BuildManager.instance.builtTowerList.Count > 0) {
+                    if (!IsConditionMeet) {
+                        OnTutorialConditionMeet();
+                    }
+                }
                 break;
+
             case Enum_TutorialState.ModeSwitching:
                 break;
             case Enum_TutorialState.TowerActivation:
@@ -170,16 +206,19 @@ public class TutorialManager : MonoBehaviour {
             case Enum_TutorialState.Start:
                 UpdateStep();
                 break;
+
             case Enum_TutorialState.PlayerMovement:
                 InputStateManager.instance.GameInputState = Enum_GameInputState.Tutorial;
                 UserInterfaceManager.instance.ChangeTextMessage(tutorialText, playerMovementTutorialMessages[playerMovementTutorialMessages.Count - 1]);
-                UserInterfaceManager.instance.ToggleUI(pressEnterToContinueText);
-                if (Input.GetKeyDown(KeyCode.Return)) {
-                    UpdateStep();
-                }
+                pressEnterToContinueText.SetActive(true);
                 break;
+
             case Enum_TutorialState.BuildCommand:
+                InputStateManager.instance.GameInputState = Enum_GameInputState.Tutorial;
+                UserInterfaceManager.instance.ChangeTextMessage(tutorialText, buildCommandTutorialMessages[buildCommandTutorialMessages.Count - 1]);
+                pressEnterToContinueText.SetActive(true);
                 break;
+
             case Enum_TutorialState.ModeSwitching:
                 break;
             case Enum_TutorialState.TowerActivation:
