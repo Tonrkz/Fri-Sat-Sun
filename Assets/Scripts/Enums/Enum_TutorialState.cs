57eef76886d0e5f23b30d50850bf8de0170f2bd2
Tonrkz <tonrak.5@hotmail.com>
2025-02-12 19:32:02 +0700

    Create Enum_TutorialState.cs

---
+++ Assets/Scripts/Enums/Enum_TutorialState.cs
@@ -0,0 +1,15 @@
+public enum Enum_TutorialState {
+    PlayerMovement,
+    BuildCommand,
+    ModeSwitching,
+    TowerActivation,
+    DestroyCommmand,
+    PlayerTest1,
+    DifferentiateCommand,
+    UpgradeCommand,
+    PlayerTest2,
+    TaxCommand,
+    GodOffering,
+    PlayerTest3,
+    End
+}
